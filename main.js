522aa38f3684ec3c89f90b826f3ad1b535621f01
Roberto Vega <rvegag95@gmail.com>
2023-11-19 01:31:48 -0300

    restart button added and minor fixes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as Phaser from "phaser";
 
-var config = {
+const config = {
   type: Phaser.AUTO,
   parent: "game",
   width: window.innerWidth > 800 ? 800 : window.innerWidth,
@@ -9,6 +9,7 @@ var config = {
   physics: {
     default: "arcade",
     arcade: {
+      debug: true,
       gravity: { y: 1000 },
     },
   },
@@ -19,10 +20,20 @@ var config = {
   },
 };
 
+const Constants = {
+  Player: {
+    VELOCITY_Y: -450,
+  },
+  Enemy: {
+    VELOCITY_X: -300,
+  },
+};
+
 // Game objects
 let ground;
 let player;
 let enemies;
+let button;
 
 // Input
 let cursors;
@@ -31,7 +42,6 @@ let pointer;
 // Score-related
 let score = 0;
 let scoreText;
-let scoreEvent;
 
 // Game state
 let gameOver = false;
@@ -39,6 +49,7 @@ let gameOverText;
 
 // Events
 let spawnEvent;
+let scoreEvent;
 
 // Sounds
 let jumpSfx;
@@ -57,6 +68,10 @@ function preload() {
     frameWidth: 24,
     frameHeight: 24,
   });
+  // this.load.spritesheet("retryButton", "assets/tilemapCharacters.png", {
+  //   frameWidth: 24,
+  //   frameHeight: 24,
+  // });
   this.load.audio("jumpSfx", "audio/sfxJump.mp3");
   this.load.audio("gameOver", "audio/gameOver.wav");
 }
@@ -82,15 +97,46 @@ function create() {
 
   // Text
   scoreText = this.add.text(15, 15, "Puntos: 0", {
-    fill: "#000",
+    fill: "#ffffff",
     font: "500 32px PixelifySans",
+    stroke: "#000000",
+    strokeThickness: 8,
   });
 
-  gameOverText = this.add.text(15, 50, "GAME OVER", {
+  gameOverText = this.add.text(0, 100, "GAME OVER", {
     font: "500 32px PixelifySans",
     fill: "#ff0000",
+    stroke: "#000000",
+    strokeThickness: 8,
   });
+
+  gameOverText.x = (this.cameras.main.width - gameOverText.width) / 2;
+  console.log(gameOver);
   gameOverText.visible = false;
+
+  button = this.add
+    .text(0, 150, "RETRY", {
+      font: "500 28px PixelifySans",
+      fill: "#ffffff",
+      stroke: "#000000",
+      strokeThickness: 8,
+      backgroundColor: "#ff0000",
+    })
+    .setInteractive();
+  button.setDepth(100);
+  button.x = (this.cameras.main.width - button.width) / 2;
+  button.visible = false;
+
+  button.on(
+    "pointerup",
+    function () {
+      if (gameOver) {
+        gameOver = false;
+        this.scene.restart();
+      }
+    },
+    this
+  );
 
   // Sound effects
   jumpSfx = this.sound.add("jumpSfx");
@@ -117,20 +163,24 @@ function create() {
   });
 
   // Animations
-  this.anims.create({
-    key: "playerWalk",
-    frames: this.anims.generateFrameNumbers("player", { start: 4, end: 5 }),
-    frameRate: 5,
-    repeat: -1,
-  });
+  if (!this.anims.get("playerWalk")) {
+    this.anims.create({
+      key: "playerWalk",
+      frames: this.anims.generateFrameNumbers("player", { start: 4, end: 5 }),
+      frameRate: 5,
+      repeat: -1,
+    });
+  }
   player.anims.play("playerWalk");
 
-  this.anims.create({
-    key: "enemyWalk",
-    frames: this.anims.generateFrameNumbers("enemy", { start: 21, end: 23 }),
-    frameRate: 5,
-    repeat: -1,
-  });
+  if (!this.anims.get("enemyWalk")) {
+    this.anims.create({
+      key: "enemyWalk",
+      frames: this.anims.generateFrameNumbers("enemy", { start: 21, end: 23 }),
+      frameRate: 5,
+      repeat: -1,
+    });
+  }
 
   // Input
   cursors = this.input.keyboard.createCursorKeys();
@@ -140,12 +190,10 @@ function create() {
   this.input.on(
     "pointerdown",
     function (pointer) {
-      if (pointer.isDown && player.body.touching.down) {
-        jumpSfx.play();
-        player.setVelocityY(-500);
-
-        if (gameOver) {
-          this.scene.restart();
+      if (!gameOver) {
+        if (pointer.isDown && player.body.touching.down) {
+          jumpSfx.play();
+          player.setVelocityY(Constants.Player.VELOCITY_Y);
         }
       }
     },
@@ -154,26 +202,24 @@ function create() {
 }
 
 function update() {
-  if (cursors.space.isDown && player.body.touching.down) {
-    jumpSfx.play();
-    player.setVelocityY(-500);
-  }
-
   console.log(gameOver);
-
-  if (pointer.isDown && player.body.touching.down) {
-    jumpSfx.play();
-    player.setVelocityY(-500);
-    if (gameOver) {
-      this.scene.restart();
+  if (!gameOver) {
+    if (cursors.space.isDown && player.body.touching.down) {
+      jumpSfx.play();
+      player.setVelocityY(Constants.Player.VELOCITY_Y);
     }
+
+    if (pointer.isDown && player.body.touching.down) {
+      jumpSfx.play();
+      player.setVelocityY(Constants.Player.VELOCITY_Y);
+    }
+
+    enemies.getChildren().forEach((enemy) => {
+      if (enemy.x <= -100) {
+        enemy.destroy();
+      }
+    });
   }
-
-  enemies.getChildren().forEach((enemy) => {
-    if (enemy.x <= -100) {
-      enemy.destroy();
-    }
-  });
 }
 
 function spawnEnemy() {
@@ -183,7 +229,7 @@ function spawnEnemy() {
     .setSize(12, 20)
     .setOffset(8, 4);
 
-  enemy.setVelocityX(-300);
+  enemy.setVelocityX(Constants.Enemy.VELOCITY_X);
   enemy.anims.play("enemyWalk");
 }
 
@@ -192,7 +238,7 @@ function spawnEnemyWithRandomDelay() {
 
   spawnEvent.remove(false);
   spawnEvent = this.time.addEvent({
-    delay: Phaser.Math.Between(800, 3000),
+    delay: Phaser.Math.Between(1000, 3000),
     callback: spawnEnemyWithRandomDelay,
     callbackScope: this,
     loop: true,
@@ -212,6 +258,7 @@ function hit() {
 
   player.setTint(0xff0000);
   gameOverText.visible = true;
+  button.visible = true;
 
   player.anims.stop();
 
